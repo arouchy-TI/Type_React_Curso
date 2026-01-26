67074456e336a0520035d68ab60b4425d20c41f8
felipe <livearouchy@gmail.com>
2026-01-26 19:40:08 -0300

    feature: limpando arquivo

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,19 +1,21 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 // Componente de Login 
 export const Login = () => {
 
     // usando use Ref
-    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
+    
     // usando use state
     const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");
 
-    const handleEntrar = () => {
+    // usando use callback
+    const handleEntrar = useCallback(() => {
         console.log(email);
         console.log(password);
-    };
+
+    }, [email, password]);
 
     // usando use effect
     useEffect(() => {
@@ -32,11 +34,13 @@ export const Login = () => {
         console.log(password);
     }, [password]);
 
-    // usando use memo
+    // usando use memo - memoriza o valor calculado
     const emailLength = useMemo(() => {
         console.log("Calculando tamanho do email...");
         return email.length * 1000;
     }, [email.length]);
+
+
     
     return(
         <div>
@@ -46,21 +50,12 @@ export const Login = () => {
 
                 <label>
                     <span>Email</span>
-                    <input value={email} 
-                            onChange={evento => setEmail(evento.target.value)} 
-                        />
-
+                    <input value={email} onChange={evento => setEmail(evento.target.value)} />
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input 
-                            type="password"
-                            value={password} 
-                            ref={inputPasswordRef}
-                            onChange={evento => setPassword(evento.target.value)} 
-                        />
-
+                    <input value={password} onChange={evento => setPassword(evento.target.value)} />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
