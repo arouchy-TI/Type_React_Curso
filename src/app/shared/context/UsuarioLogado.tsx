91375ff40d4561a449a24b798233161642b9f325
felipe <livearouchy@gmail.com>
2026-02-02 13:54:35 -0300

    feature: ajustando entendimento sobre o context

--- src/app/shared/context/UsuarioLogado.tsx
+++ src/app/shared/context/UsuarioLogado.tsx
@@ -1,16 +1,19 @@
 import React, { createContext } from "react";
 
+// dados compartilhados no contexto
 interface IUsuarioLogadoContextData {
     nomeUsuario: string;
 };
 
-// criando canal de transmissao - subcomponenetes (provider, consumer)
+// criando canal de transmissao - subcomponenetes (provider, consumer) - parametros de tipagem
+// objeto vazio {} - representacao IUsuarioLogadoContextData
 const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 interface IUsuarioLogadoProviderProps {
     children: React.ReactNode
 }
 
+// contante do tipo, componente react funcional
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ( {children} ) => {
     return(
         <UsuarioLogadoContext.Provider value={{nomeUsuario: "Felipe"}}>
