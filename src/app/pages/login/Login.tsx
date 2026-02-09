2fc6a9081bb91fe5453fd1a8b6c16a085939e960
felipe <livearouchy@gmail.com>
2026-02-09 00:54:12 -0300

    FIX: Ajustando Arquivos para rodar na main

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,50 +1,25 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-<<<<<<< HEAD
 import { InputLogin } from "./components/InputLogin";
 import { ButtonLoggin } from "./components/ButtonLogin";
 import { useUsuarioLogado } from "../../shared/hooks";
 
-
 // Componente de Login 
 export const Login = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
-    // usando use Context - varias partes da aplicacao
     const {nomeUsuario, logout} = useUsuarioLogado();
-
-    // usando use Ref
-    const inputPasswordRef = useRef<HTMLInputElement>(null);
->>>>>>> feature/LoginComponents
     
-=======
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
->>>>>>> feature/atualizando_documentacao
-=======
-
-// Componente de Login 
-export const Login = () => {
-
->>>>>>> feature/useCallBack
     // usando use state
     const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");
 
     // usando use callback
-<<<<<<< HEAD
-    const handleEntrar = useCallback( () => {
-        console.log(email);
-        console.log(password);
-=======
     const handleEntrar = useCallback(() => {
         console.log(email);
         console.log(password);
 
     }, [email, password]);
->>>>>>> feature/useCallBack
-
-    }, [email, password] );
 
     // usando use effect - mensagem aparecendo na tela roda apenas uma vez
     useEffect(() => {
@@ -64,11 +39,7 @@ export const Login = () => {
         console.log(password);
     }, [password]);
 
-<<<<<<< HEAD
-    // usando use memo - memoriza o valor calculado, guarda um valor 
-=======
     // usando use memo - memoriza o valor calculado
->>>>>>> feature/useCallBack
     const emailLength = useMemo(() => {
         console.log("Calculando tamanho do email...");
         return email.length * 1000;
@@ -83,38 +54,6 @@ export const Login = () => {
                 <p>Quantidade de caracteres no email: {emailLength}</p>
                 <p>Usuario Logado: {nomeUsuario}</p>
 
-<<<<<<< HEAD
-                <label>
-                    <span>Email</span>
-                    <input 
-                        value={email} 
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        onChange={evento => setEmail(evento.target.value)}
-=======
-                        onChange={evento => setEmail(evento.target.value)} 
->>>>>>> feature/atualizando_documentacao
-=======
-                        onChange={evento => setEmail(evento.target.value)} 
->>>>>>> feature/useCallBack
-                        />
-                </label>
-
-                <label>
-                    <span>Senha</span>
-<<<<<<< HEAD
-                    <input 
-                        value={password} 
-                        onChange={evento => setPassword(evento.target.value)} 
-=======
-                    <input
-                        type="password" 
-                        value={password}
-                        onChange={evento => setPassword(evento.target.value)}
->>>>>>> feature/useCallBack
-                        />
-                </label>
-=======
                 {/* importando componente de InputLogin da pasta login/components */}
                 <InputLogin
                     label="Email"
@@ -130,7 +69,7 @@ export const Login = () => {
                     value={password}
                     onChange={newValue => setPassword(newValue)}
                 />
->>>>>>> feature/LoginComponents
+
 
                 <ButtonLoggin type="button" onClick={handleEntrar}>
                     Entrar
--- src/app/shared/context/UsuarioLogado.tsx
+++ src/app/shared/context/UsuarioLogado.tsx
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import React, { createContext, useCallback } from "react";
 
 // dados compartilhados no contexto - propiedades que vao poder ser disponibilizadas no contexto
 interface IUsuarioLogadoContextData {
     nomeUsuario: string;
     logout: () => void;
-=======
-import React, { createContext } from "react";
-=======
-import React, { createContext, useCallback } from "react";
->>>>>>> feature/hooks
-
-// dados compartilhados no contexto - propiedades que vao poder ser disponibilizadas no contexto
-interface IUsuarioLogadoContextData {
-    nomeUsuario: string;
-<<<<<<< HEAD
->>>>>>> feature/contexts
-=======
-    logout: () => void;
->>>>>>> feature/hooks
 };
 
 // criando canal de transmissao - subcomponenetes (provider, consumer) - parametros de tipagem
@@ -32,28 +17,14 @@ interface IUsuarioLogadoProviderProps {
 
 // passando dado: nomeUsuario, para os filhos do provider 
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ( {children} ) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    // crianca funcao para ser usada no contexto
-=======
-
->>>>>>> feature/hooks
+    // criando funcao para ser usada no contexto
     const handlerLogout = useCallback(() => {
         console.log('Testando botao');
     }, []);
 
-<<<<<<< HEAD
     return(
         <UsuarioLogadoContext.Provider value={{ nomeUsuario: "Felipe", logout: handlerLogout }}>
-=======
-    return(
-        <UsuarioLogadoContext.Provider value={{nomeUsuario: "Felipe"}}>
->>>>>>> feature/contexts
-=======
-    return(
-        <UsuarioLogadoContext.Provider value={{ nomeUsuario: "Felipe", logout: handlerLogout }}>
->>>>>>> feature/hooks
             {children}
         </UsuarioLogadoContext.Provider>
     );
